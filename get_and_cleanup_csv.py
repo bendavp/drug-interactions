0ed986d3daba6ead1ffb2d17814a7cac56a0c680
bendavp <bkosiborod@gmail.com>
2021-04-13 18:40:59 -0400

    cleanup script v2 done, MUCH faster now. Added .gitignore

--- get_and_cleanup_csv.py
+++ get_and_cleanup_csv.py
@@ -1,119 +1,109 @@
 import pandas as pd
-import requests
-import time
 import pubchempy as pcp
-
-# Import drug-disease csv
-print('Importing drug_disease CSV (1/3)...')
-drug_disease = pd.read_csv(
-    'https://snap.stanford.edu/biodata/datasets/10004/files/DCh-Miner_miner-disease-chemical.tsv.gz',
-    delimiter='\t', names=['Disease ID', 'Drug ID'], skiprows=1)
-
-# Initialize the drug name and disease name columns
-drug_disease['Disease Name'] = drug_disease['Disease ID']
-drug_disease['Drug Name'] = drug_disease['Drug ID']
-
-# Log initial length of DataFrame
-print('Beginning drug/disease name retrieval for drug_disease CSV (1/3)...')
-initial_length = len(drug_disease)
-
-'''Loop through each row in the DataFrame, querying for the drug id and name,
-and disease name at each row. If any error occurs, save time by simply dropping
-the row. We should have plenty of data to play with regardless...
-
-Note, for this loop, we are querying both PubChem (via PubChemPy) AND the MeSH
-RESTful API Since the given drug IDs are not the standardized compound IDs (cid)
-as seen in the next two DataFrames, find the cid of the drug first, replace the
-given ID with the found cid (for easy joining when importing into neo4J), then
-get that drug's name/synonym.'''
-for idx, disease_id, drug_id in zip(drug_disease.index, drug_disease['Disease ID'], drug_disease['Drug ID']):
-    # to make sure MeSH querying doesn't time out due to too many requests
-    time.sleep(0.2)
-    try:
-        drug_disease['Disease Name'][idx] = requests.get(
-            f"https://id.nlm.nih.gov/mesh/lookup/details?descriptor={disease_id[5:]}&includes=terms").json()['terms'][0]['label']
-        drug_data = pcp.get_compounds(drug_id, 'name')[0]
-        drug_disease['Drug ID'][idx] = drug_data.cid
-        drug_disease['Drug Name'][idx] = drug_data.synonyms[0]
-    except (IndexError):
-        drug_disease.drop(labels=idx, axis='index', inplace=True)
-
-# Log the final length of the DataFrame, to see how much data was lost
-print('Done with drug/disease name retrieval for drug_disease CSV (1/3)!')
-print('Number of lost rows in drug_disease DataFrame:',
-      initial_length - len(drug_disease))
-
-# Save the csv
-drug_disease.to_csv('drug_disease.csv')
-print('drug_disease CSV saved (1/3)!')
+import time
+import requests
 
 # Import drug-drug-side_effect csv
-print('Importing drug_interaction_side_effects CSV (2/3)...')
+print("Importing drug-drug-side_effect CSV...")
 drug_interaction_side_effects = pd.read_csv(
     'https://snap.stanford.edu/biodata/datasets/10017/files/ChChSe-Decagon_polypharmacy.csv.gz',
     names=['Drug 1 ID', 'Drug 2 ID', 'Side Effect ID', 'Side Effect Name'], skiprows=1)
 
-# Log the initial length of the DataFrame
-print('Beginning drug name retrieval for drug_interaction_side_effects CSV (2/3)...')
-initial_length = len(drug_interaction_side_effects)
-
-# Initialize both of the drug name columns
-drug_interaction_side_effects['Drug 1 Name'] = drug_interaction_side_effects['Drug 1 ID']
-drug_interaction_side_effects['Drug 2 Name'] = drug_interaction_side_effects['Drug 2 ID']
-
-# Loop through each row in the DataFrame, querying for both drug names at each
-# row. If any error occurs, save time by simply dropping the row. We should have
-# plenty of data to play with regardless...
-for idx, drug1_id, drug2_id in zip(drug_interaction_side_effects.index,
-                                   drug_interaction_side_effects['Drug 1 ID'],
-                                   drug_interaction_side_effects['Drug 2 ID']):
-    try:
-        drug_interaction_side_effects['Drug 1 Name'][idx] = pcp.Compound.from_cid(
-            int(drug1_id[3:])).synonyms[0]
-        drug_interaction_side_effects['Drug 2 Name'][idx] = pcp.Compound.from_cid(
-            int(drug2_id[3:])).synonyms[0]
-    except (IndexError):
-        drug_interaction_side_effects.drop(
-            labels=idx, axis='index', inplace=True)
-
-# Log the final length of the DataFrame, to see how much data was lost
-print('Done with drug name retrieval for drug_interaction_side_effects CSV (2/3)!')
-print('Number of lost rows in drug_interaction_side_effects DataFrame:',
-      initial_length - len(drug_interaction_side_effects))
-
-# Save the csv
-drug_interaction_side_effects.to_csv('drug_interaction_side_effects.csv')
-print('drug_interaction_side_effects CSV saved (2/3)!')
-
 # Import drug-side_effect csv
-print('Importing drug_side_effects CSV (3/3)...')
+print("Importing drug-side_effect CSV...")
 drug_side_effects = pd.read_csv(
     'https://snap.stanford.edu/biodata/datasets/10018/files/ChSe-Decagon_monopharmacy.csv.gz',
     names=['Drug ID', 'Side Effect ID', 'Side Effect Name'], skiprows=1)
 
-# Initialize the drug name column
-drug_side_effects['Drug Name'] = drug_side_effects['Drug ID']
+# Get all unique PubMed compound IDs from above datasets
+all_drug_ids = set([*drug_side_effects['Drug ID'].unique(),
+                    *drug_interaction_side_effects['Drug 1 ID'].unique(),
+                    *drug_interaction_side_effects['Drug 2 ID'].unique()])
 
-# Log initial length of DataFrame
-print('Beginning drug name retrieval for drug_side_effects CSV (3/3)...')
-initial_length = len(drug_side_effects)
+# Initialize empty dictionary to store ID --> name mapping
+drug_id_names = {}
 
-# Loop through each row in the DataFrame, querying for the drug name at each row
-# If any error occurs, save time by simply dropping the row. We should have
-# plenty of data to play with regardless...
-for idx, drug_id in zip(drug_side_effects.index, drug_side_effects['Drug ID']):
+# Query PubMed for name of each unique drug/compound ID
+counter = 1
+for drug_id in all_drug_ids:
+    print("Processing drug #", counter, "/", len(all_drug_ids))
+    counter += 1
+
+    # To prevent PubMed from timing us out. Apparently this is NOT built into
+    # the PubMed RESTful API wrapper...
+    time.sleep(0.2)
     try:
-        drug_side_effects['Drug Name'][idx] = pcp.Compound.from_cid(
-            int(drug_id[3:])).synonyms[0]
+        drug_id_names[drug_id] = pcp.Compound.from_cid(drug_id[3:]).synonyms[0]
+    # If the synonyms list is empty, then most likely PubMed did not return a
+    # valid compound corresponding to our ID. So let's take the easy route and
+    # just remove the offending compound ID from our data. We'll still have
+    # plenty left to play with...
     except (IndexError):
-        drug_side_effects.drop(labels=idx, axis='index', inplace=True)
+        drug_interaction_side_effects = drug_interaction_side_effects[(drug_interaction_side_effects['Drug 1 ID']
+                                                                       != drug_id) & (drug_interaction_side_effects['Drug 2 ID'] != drug_id)]
+        drug_side_effects = drug_side_effects[drug_side_effects['Drug ID'] != drug_id]
 
-# Log the final length of the DataFrame, to see how much data was lost
-print('Done with drug/disease name retrieval for drug_side_effects CSV (3/3)!')
-print('Number of lost rows in drug_side_effects DataFrame:',
-      initial_length - len(drug_side_effects))
+# Apply the mapping to each drug column in our data.
+print("All drug names fetched successfully! Applying to drug-side_effect and drug-drug-side_effect CSVs...")
+drug_interaction_side_effects['Drug 1 Name'] = drug_interaction_side_effects['Drug 1 ID'].apply(
+    lambda drug: drug_id_names[drug])
+drug_interaction_side_effects['Drug 2 Name'] = drug_interaction_side_effects['Drug 2 ID'].apply(
+    lambda drug: drug_id_names[drug])
+drug_side_effects['Drug Name'] = drug_side_effects['Drug ID'].apply(
+    lambda drug: drug_id_names[drug])
+
+print("Saving both CSVs...")
+drug_interaction_side_effects.to_csv('drug_interaction_side_effects.csv')
+drug_side_effects.to_csv('drug_side_effects.csv')
+print("drug-side_effect and drug-drug-side_effect CSVs saved successfully!")
+
+# Import drug-disease csv
+print('Importing drug_disease CSV...')
+drug_disease = pd.read_csv(
+    'https://snap.stanford.edu/biodata/datasets/10004/files/DCh-Miner_miner-disease-chemical.tsv.gz',
+    delimiter='\t', names=['Disease ID', 'Drug ID'], skiprows=1)
+
+all_ncbi_drug_ids = set(drug_disease['Drug ID'])
+all_disease_ids = set(drug_disease['Disease ID'])
+
+drug_id_cid_names = {}
+counter = 1
+for ncbi_drug_id in all_ncbi_drug_ids:
+    print("Processing drug #", counter, "/", len(all_drug_ids))
+    counter += 1
+    time.sleep(0.2)
+    try:
+        # Get appropriate compound based on NCBI ID
+        drug_info = pcp.get_compounds(ncbi_drug_id, 'name')[0]
+        # Get PubMed compound ID in the same format as previous two datasets
+        drug_id_cid_names[ncbi_drug_id] = ("CID" + str(drug_info.cid).zfill(8),
+                                           drug_info.synonyms[0])
+    except (IndexError):
+        drug_disease = drug_disease[drug_disease['Drug ID'] != ncbi_drug_id]
+
+print("All drug names fetched successfully! Applying to drug-disease CSV...")
+drug_disease['Drug Name'] = drug_disease['Drug ID'].apply(
+    lambda drug: drug_id_cid_names[drug][1])
+drug_disease['Drug ID'] = drug_disease['Drug ID'].apply(
+    lambda drug: drug_id_cid_names[drug][0])
+
+disease_id_names = {}
+counter = 1
+for disease_id in all_disease_ids:
+    print("Processing disease #", counter, "/", len(all_disease_ids))
+    counter += 1
+    time.sleep(0.2)
+    try:
+        disease_id_names[disease_id] = requests.get(
+            f"https://id.nlm.nih.gov/mesh/lookup/details?descriptor={disease_id[5:]}&includes=terms").json()['terms'][0]['label']
+    except (IndexError):
+        drug_disease = drug_disease[drug_disease['Disease ID'] != disease_id]
+
+print("All disease names fetched successfully! Applying to drug-disease CSV...")
+drug_disease['Disease Name'] = drug_disease['Disease ID'].apply(
+    lambda disease: disease_id_names[disease])
 
 # Save the csv
-drug_side_effects.to_csv('drug_side_effects.csv')
-print('drug_side_effects CSV saved (3/3)!')
-print('All CSV processing done!')
+print("Saving drug-disease CSV...")
+drug_disease.to_csv('drug_disease.csv')
+print("CSV saved successfully, all done!")
